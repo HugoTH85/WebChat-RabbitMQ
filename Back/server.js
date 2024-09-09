56f791e2355c146c6478e8a63e8c208c5bd57308
Hugo <tricoire@et.esiea.fr>
2024-09-09 17:05:51 +0200

    UP log des messages envoyés

--- Back/server.js
+++ Back/server.js
@@ -9,6 +9,19 @@ const io = socketIo(server);
 
 const EXCHANGE = 'direct_exchange';
 
+function getLogDate() {
+    const now = new Date();
+
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Mois de 01 à 12
+    const day = String(now.getDate()).padStart(2, '0'); // Jour du mois
+    const hours = String(now.getHours()).padStart(2, '0'); // Heures
+    const minutes = String(now.getMinutes()).padStart(2, '0'); // Minutes
+    const seconds = String(now.getSeconds()).padStart(2, '0'); // Secondes
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/chat/:user', (req, res) => {
@@ -38,7 +51,7 @@ async function setup() {
             const { message, user } = data;
             const routingKey = user === 'user1' ? 'user2_key' : 'user1_key';
             channel.publish(EXCHANGE, routingKey, Buffer.from(message));
-            console.log(`Message sent: ${message}`);
+            console.log(`[${getLogDate()}] ${user} : ${message}`);
 
             // Broadcast the message to all clients
             io.emit('receive_message', { msg: message, time: new Date().toLocaleTimeString(), user });
